e2f05701823be5602aaa03808db67fc7e7695e15
Alessandro de Cristo Moleta <90357593+alemoleta@users.noreply.github.com>
2021-10-11 15:46:45 -0300

    Add files via upload

---
+++ pagina.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Aplicação de jQuery UI Accordion</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Seção 1</h3>
+  <div>
+    <p>
+    Teste seção 1. 
+    Balblablablablablbalbslalsdlasdnlkasnkdlam
+    </p>
+  </div>
+  <h3>Seção 2</h3>
+  <div>
+    <p>
+    Seção 2....
+    aojdoiajsodaj osd joijsdajsodsadjlas  ao aoid asjda
+    </p>
+  </div>
+  <h3>Seção 3</h3>
+  <div>
+    <p>
+    Seção 3....
+    osajfdoa jsoifa sdjfandsofa ndona fnadsm
+    </p>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+    </ul>
+  </div>
+  
+</div>
+ 
+ 
+</body>
+</html>
